08a38f19660f3faab498712999711ca3df01a73f
williamwangwei <2840769859@qq.com>
2016-07-26 01:11:07 +0800

    Signed-off-by: williamwangwei <2840769859@qq.com>

---
+++ ww-manager-pojo/src/main/java/org/ww/manager/pojo/App.java
@@ -0,0 +1,13 @@
+package org.ww.manager.pojo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
